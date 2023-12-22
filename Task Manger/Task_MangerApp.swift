adedc78f1bc1d86b2593eb70f85ec03189abd228
amalalreshi <amal.alreshi@gmail.com>
2023-12-22 16:15:27 +0300

    Initial Commit

---
+++ Task Manger/Task_MangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_MangerApp.swift
+//  Task Manger
+//
+//  Created by Amal Mohammad Alreshi on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Task_MangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
